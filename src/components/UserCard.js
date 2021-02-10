6c038414760297cdafcddfedf1590e20f8030a7b
jglez <jglez.cs@gmail.com>
2021-02-09 19:36:23 -0600

    Rendering GitHub User information from state

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,18 +1,31 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 const UserCard = () => {
   const [user, setUser] = useState('')
 
-  axios
-    .get(`https://api.github.com/users/jglez`)
-    .then(res => {
-      console.log(res)
-    })
+  // We're scheduling whatever code inside of the callback to run after the first render cycle
+  // When this component is done being painted in the DOM, this code runs
+  useEffect(() => {
+    axios
+      .get('https://api.github.com/users/jglez')
+      .then(res => {
+        setUser(res.data)
+        console.log(user)
+      })
+      .catch(err => {
+        console.log(`Error is ${err}`)
+      })
+  }, [])
+
 
   return (
     <div>
       <h1>GitHub User Card</h1>
+      <img src={user.avatar_url} alt='' />
+      <h2><b>Name:</b> {user.name}</h2>
+      <h2><b>Location:</b> {user.location}</h2>
+      <p><b>Bio:</b> {user.bio}</p>
     </div>
   )
 }
